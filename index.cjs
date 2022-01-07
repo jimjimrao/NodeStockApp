c2cba4196f75bfe4a6e2188a55a5c93de5830adb
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2022-01-06 22:04:47 -0800

    set up handlebars directory

---
+++ index.cjs
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// Set handlebar routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('Server Listening on port' + PORT)) ;
