64541389f286e38c58db8d6d126b6808f94eab08
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2022-01-07 00:30:05 -0800

    configured the search function to show stats for searched ticker

--- index.cjs
+++ index.cjs
@@ -3,15 +3,18 @@ const exphbs  = require('express-handlebars');
 const app = express();
 const path = require('path');
 const request = require('request');
-
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
 // APIk3y pk_4ff4c2aeffc3428e9204ed61ae9cc776
 
 const key = 'pk_4ff4c2aeffc3428e9204ed61ae9cc776';
 // Call API function
-function call_api(finishedAPI){
-    request('https://cloud.iexapis.com/stable/stock/aapl/quote?token=' + key, {json: true}, (err, res, body) => {
+function call_api(finishedAPI,ticker){
+    request('https://cloud.iexapis.com/stable/stock/'+ticker+'/quote?token=' + key, {json: true}, (err, res, body) => {
         if (err) {return console.log(err);}
         if (res.statusCode === 200){
             // console.log(body);
@@ -25,13 +28,23 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 const otherstuff = "hello!!!!";
-// Set handlebar routes
+// Set handlebar index GET route
 app.get('/', (req, res) => {
     call_api(function(doneAPI){
         res.render('home',{
             stock: doneAPI
         });
     });
+});
+
+// Set handlebar index POST route
+app.post('/', (req, res) => {
+    call_api(function(doneAPI){
+        // posted_stuff = req.body.stock_ticker
+        res.render('home',{
+            stock: doneAPI,
+        });
+    },req.body.stock_ticker);
 });
 
 // create about page route
