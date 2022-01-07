bc046b4a2f93fbe4af3db24aa0dd17215441def6
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2022-01-06 21:23:44 -0800

    basic static website up and runnning

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000; 
 
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log('Server Listening on port' + PORT)) ;
